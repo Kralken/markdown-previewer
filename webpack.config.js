b7575e365a9e8df4a74678437a7811f2a876797c
Clark <clarkenreyes@gmail.com>
2023-05-12 19:44:09 +0800

    added gh pages and settings to publish in gh pages

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './index.js',
   mode: 'development',
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, './build'),
     filename: 'index_bundle.js',
   },
   target: 'web',
